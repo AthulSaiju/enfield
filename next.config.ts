2914483cb805f2a77c0ae7c95d3054cf0e1f101d
toughShit <athulsaiju02@gmail.com>
2025-09-22 22:44:15 +0530

    chore(nextjs): disable ESLint checks during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ✅ This will ignore ESLint errors during builds (local + Vercel)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
